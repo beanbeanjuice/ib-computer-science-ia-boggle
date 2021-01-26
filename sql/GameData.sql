6efdbb88ecfea42be8cefd79bd7b5ae20bdb9201
William <64740551+beanbeanjuice@users.noreply.github.com>
2021-01-26 07:13:31 -0500

    Changed

--- sql/GameData.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE GameData
-(GameID INT,
-Score INT,
-BoardCharacters NVARCHAR(100),
-TimeTaken INT,
-TimeAllowed INT,
-WordsFound INT,
-TotalWords INT);
---
+++ sql/tables/Settings.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Settings
+(ID INT,
+TimeLimit INT,
+IgnoreTimeLimit INT);
